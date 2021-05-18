d45dbc6a10480b4c50ea40fd1a59d86fd05dc77a
Tony-L-93 <tonyliendro@gmail.com>
2021-05-17 22:38:16 -0300

    Commit to change branch

--- src/test/java/click/escuela/grade/controller/GradeControllerTest.java
+++ src/test/java/click/escuela/grade/controller/GradeControllerTest.java
@@ -64,8 +64,8 @@ public class GradeControllerTest {
 		schoolId = "1234";
 		studentId = UUID.randomUUID().toString();
 		courseId = UUID.randomUUID().toString();
-		gradeApi = GradeApi.builder().name("Examen").subject("Matematica").studentId(studentId).type(GradeType.HOMEWORK.toString()).courseId(courseId)
-				.number(10).build();
+		gradeApi = GradeApi.builder().name("Examen").subject("Matematica").studentId(studentId)
+				.type(GradeType.HOMEWORK.toString()).courseId(courseId).number(10).build();
 
 		doNothing().when(gradeService).create(Mockito.any());
 	}
@@ -73,9 +73,8 @@ public class GradeControllerTest {
 	@Test
 	public void whenCreateIsOk() throws JsonProcessingException, Exception {
 
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
 				.andExpect(status().is2xxSuccessful()).andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains(GradeEnum.CREATE_OK.name());
@@ -86,10 +85,9 @@ public class GradeControllerTest {
 	public void whenCreateButNameEmpty() throws JsonProcessingException, Exception {
 
 		gradeApi.setName(EMPTY);
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Name cannot be empty");
 
@@ -99,10 +97,9 @@ public class GradeControllerTest {
 	public void whenCreateButSubjectEmpty() throws JsonProcessingException, Exception {
 
 		gradeApi.setSubject(EMPTY);
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Subject cannot be empty");
 
@@ -113,24 +110,22 @@ public class GradeControllerTest {
 
 		gradeApi.setCourseId(EMPTY);
 		;
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Course cannot be empty");
 
 	}
-	
+
 	@Test
 	public void whenCreateButStudentEmpty() throws JsonProcessingException, Exception {
 
 		gradeApi.setStudentId(EMPTY);
 		;
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Student cannot be empty");
 
@@ -140,10 +135,9 @@ public class GradeControllerTest {
 	public void whenCreateButTypeEmpty() throws JsonProcessingException, Exception {
 
 		gradeApi.setType(EMPTY);
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Type cannot be empty");
 
@@ -153,10 +147,9 @@ public class GradeControllerTest {
 	public void whenCreateButNumberNull() throws JsonProcessingException, Exception {
 
 		gradeApi.setNumber(null);
-		MvcResult result = mockMvc
-				.perform(post("/school/{schoolId}/grade", schoolId)
-						.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi)))
-				.andExpect(status().isBadRequest()).andReturn();
+		MvcResult result = mockMvc.perform(post("/school/{schoolId}/grade", schoolId)
+				.contentType(MediaType.APPLICATION_JSON).content(toJson(gradeApi))).andExpect(status().isBadRequest())
+				.andReturn();
 		String response = result.getResponse().getContentAsString();
 		assertThat(response).contains("Number cannot be null");
 
