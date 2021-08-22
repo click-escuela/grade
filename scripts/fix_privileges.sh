e515e6984bbda7339d6416b8b0fd4b9a8bff081c
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-21 23:36:43 -0300

    actualizacion de scripts2

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -5,4 +5,3 @@ chmod +x /home/ec2-user/server/grade/server_start.sh
 chmod +x /home/ec2-user/server/grade/server_stop.sh
 chmod +x /home/ec2-user/server/grade/server_clear.sh
 chmod +x /home/ec2-user/server/grade/fix_privileges.sh
-chmod +x /home/ec2-user/server/grade/grade-service.pid
