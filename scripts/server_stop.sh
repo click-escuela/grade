83f5b95b8efd2e5384d3b0723f4e1f241c933266
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-21 22:34:21 -0300

    actualizacion de scripts

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo kill -9 $(sudo lsof -t -i:8091)
+var="$(cat /home/ec2-user/server/grade/grade-service.pid)"
+sudo kill $var
+sudo rm -rf /home/ec2-user/server/grade/grade-service.pid
