14a3ef060c7085536f4221174a2ae853c38d23bf
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 18:22:40 -0300

    logs fix

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -5,3 +5,6 @@ chmod +x /home/ec2-user/server/grade/server_start.sh
 chmod +x /home/ec2-user/server/grade/server_stop.sh
 chmod +x /home/ec2-user/server/grade/server_clear.sh
 chmod +x /home/ec2-user/server/grade/fix_privileges.sh
+chmod +x /home/ec2-user/server/grade/logs
+chmod +x /home/ec2-user/server/grade/logs/error.log
+chmod +x /home/ec2-user/server/grade/logs/debug.log
