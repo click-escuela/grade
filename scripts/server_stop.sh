a4e808f67db5ea9216f951762ee3bda9e10d1eb3
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 21:03:07 -0300

    fix logs

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/grade/logs
+chmod +x /home/ec2-user/server/grade/logs/error.log
+chmod +x /home/ec2-user/server/grade/logs/debug.log
 var="$(cat /home/ec2-user/server/grade/grade-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/grade/grade-service.pid
