17d3550a3808ef5447e52bf64ddc1de9934e5a9f
Tony-L-93 <tonyliendro@gmail.com>
2021-09-17 00:26:47 -0300

    Create grades was changed

--- src/main/java/click/escuela/grade/api/GradeCreateApi.java
+++ src/main/java/click/escuela/grade/api/GradeCreateApi.java
@@ -1,5 +1,7 @@
 package click.escuela.grade.api;
 
+import java.util.List;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,15 +24,13 @@ import lombok.experimental.SuperBuilder;
 @JsonInclude(Include.NON_EMPTY)
 @Schema(description = "Grade Api")
 @SuperBuilder
-public class GradeApi {
+public class GradeCreateApi {
 	
 	@JsonProperty(value = "id", required = false)
 	private String id;
 
-	@NotBlank(message = "Student cannot be empty")
-	@Size(max = 50, message = "Student must be less than 50 characters")
 	@JsonProperty(value = "studentId", required = true)
-	private String studentId;
+	private List<String> studentId;
 
 	@NotBlank(message = "Name cannot be empty")
 	@Size(max = 50, message = "Name must be less than 50 characters")
@@ -56,8 +56,7 @@ public class GradeApi {
 	@JsonProperty(value = "type", required = true)
 	private String type;
 
-	@NotNull(message = "Number cannot be null")
 	@JsonProperty(value = "number", required = true)
-	private Integer number;
+	private List<Integer> number;
 
 }
--- src/main/java/click/escuela/grade/service/GradeServiceGeneric.java
+++ src/main/java/click/escuela/grade/service/GradeServiceGeneric.java
@@ -5,8 +5,6 @@ import java.util.List;
 import click.escuela.grade.exception.TransactionException;
 
 public interface GradeServiceGeneric<T, S> {
-
-	public void create(T entity) throws TransactionException;
 	
 	public void update(T entity) throws TransactionException;
 
