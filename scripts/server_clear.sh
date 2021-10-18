d22e4011705dc1760737b12025c4b7b870d30ce6
Evelin <eve_aragon@hotmail.com>
2021-10-17 22:48:51 -0300

    mejora de scripts

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/grade
+sudo rm -rf /home/ec2-user/server/grade/appspec.yml
+sudo rm -rf /home/ec2-user/server/grade/grade-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/grade/grade-service.pid
+sudo rm -rf /home/ec2-user/server/grade/logs
+sudo rm -rf /home/ec2-user/server/grade/server_stop.sh
+sudo rm -rf /home/ec2-user/server/grade/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/grade/server_prestart.sh
+sudo rm -rf /home/ec2-user/server/grade/server_clear.sh
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/grade
 
-sudo rm -rf /home/ec2-user/server/grade/grade-service.pid
-
-echo "Eliminando archivo"
-
-sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 -Dlogging.file.name=/home/ec2-user/server/grade/debug.log \
-    grade-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > grade-service.pid
+sudo sh server_start.sh
